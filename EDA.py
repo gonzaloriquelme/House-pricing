3acdf5e320e36842a8b3c63eab2aed9295f9c4b3
gonzaloriquelme <gjavierf@gmail.com>
2019-09-05 17:20:13 -0400

    Mejoras 1

--- EDA.py
+++ EDA.py
@@ -10,34 +10,62 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 #from funciones_ayuda import find_missing_values
 from sklearn.preprocessing import StandardScaler
-from scipy.stats import norm
+from scipy.stats import norm,skew
 from scipy import stats
 
-
-
+## Cargar datos TRAIN ###
 
 df_train = pd.read_csv("train.csv", encoding ="latin-1", sep=",", header=0)
 
-
+## Códigos BÁSICOS VISTA GENERAL DE LOS DATOS
 print(df_train.columns) #Entrega todos los nombres de las variables
     
 df_train.info() #Muestra el tipo (int64, object,etc) nombre columna, non null data
     
-df_train.head() #En este caso con muchas columnas no es muy útil (muestra las 10 primeras filas)
-    
-#Diccionario de missing values por columna    
-#missing_values= find_missing_values(df_train, columns=df_train.columns)
+df_train.head() #En este caso con muchas columnas no es muy útil (muestra las 10 primeras filas)    
 
 df_train['SalePrice'].describe() #entrega medidas de tendencia central y dispersión
 
 sns.distplot(df_train['SalePrice']) #histograma o distribución de los datos ordenados
 
-print("Skewness: %f" % df_train['SalePrice'].skew())
-print("Kurtosis: %f" % df_train['SalePrice'].kurt()) 
+print("Skewness: %f" % df_train['SalePrice'].skew()) # Simetría entre  {-0,5 y 0,5} es bastante simetrico, |skew|>1 es muy skewed
+print("Kurtosis: %f" % df_train['SalePrice'].kurt()) # Kurt=3 normal Kurt>3 mucho outlier Kurt<3 poco outlier
+
+y = df_train.SalePrice
+y=np.log(y)
+### Vista de NULL VALUES
 
 nulltrain=df_train.isnull().sum()
 nulltrain=nulltrain[nulltrain>0]
 nulltrain.sort_values(ascending=False)
+
+### Se toman las variables NÚMERICAS (puede incluir categóricas)
+colsnum=df_train.select_dtypes(exclude = ["object"]).columns
+colsnum= colsnum.drop('SalePrice')
+x_num=df_train[colsnum]
+
+### Se ordenan según skewness
+skewness = x_num.apply(lambda x: skew(x))
+skewness.sort_values(ascending=False)
+skewness = skewness[abs(skewness)>0.5]
+
+#Analizar todos los histogramas y scatter de x_num con SalePrice
+for item in skewness.index.values:
+    sns.distplot(x_num[item])
+    plt.show()
+    data =pd.concat([df_train.SalePrice,x_num[item]],axis=1)
+    data.plot.scatter(y='SalePrice',x=item)
+    plt.show()
+    
+# Separación de variables NO-NÚMERICAS/CATEGÓRICAS
+colscat=df_train.select_dtypes(include = ["object"]).columns
+x_cat = df_train[colscat]
+# Analizar Boxplots de x_cat con SalePrice
+for item in colscat:
+    data = pd.concat([df_train['SalePrice'], df_train[item]], axis=1)
+    f, ax = plt.subplots(figsize=(8, 6))
+    fig = sns.boxplot(x=item, y="SalePrice", data=data)
+    plt.show()    
 
 #correlation matrix
 corrmat = df_train.corr()
@@ -52,7 +80,6 @@ sns.set(font_scale=1)
 hm = sns.heatmap(cm, cbar=True, annot=True, square=True, fmt='.2f', annot_kws={'size': 10}, yticklabels=cols.values, xticklabels=cols.values, cmap='RdYlGn')
 plt.show()   
 
-
 #Ordenamos la matriz de corr con la columna de SalePrice
 #Luego se imprime la columna
 corrmat.sort_values(['SalePrice'],ascending=False,inplace=True) #Corresponde a una accion sobre corrmat
@@ -64,24 +91,7 @@ numerical_features = numerical_features.drop("SalePrice")
 train_num = df_train[numerical_features]
 train_cat = df_train[categorical_features]
 
-
-#scatter plot grlivarea/saleprice
-var = 'GrLivArea'
-data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
-data.plot.scatter(x=var, y='SalePrice', ylim=(0,800000));
-#scatter plot totalbsmtsf/saleprice
-var = 'TotalBsmtSF'
-data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
-data.plot.scatter(x=var, y='SalePrice', ylim=(0,800000));
-
-#box plot overallqual/saleprice
-var = 'OverallQual'
-data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
-f, ax = plt.subplots(figsize=(8, 6))
-fig = sns.boxplot(x=var, y="SalePrice", data=data)
-fig.axis(ymin=0, ymax=800000);
-
-
+##### OTROS CÓDIGOS ###########
 #scatterplot
 sns.set()
 cols = ['SalePrice', 'OverallQual', 'GrLivArea', 'GarageCars', 'TotalBsmtSF', 'FullBath', 'YearBuilt']
--- housep.py
+++ housep.py
@@ -17,99 +17,118 @@ import xlsxwriter
 from scipy.stats import norm,skew
 from scipy import stats
 from funciones_ayuda import mean_absolute_percentage_error, root_mean_squared_log_error
-
+from tabulate import tabulate
 
 from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold, cross_val_score
 from sklearn.linear_model import RidgeCV
 from sklearn.metrics import mean_squared_error, r2_score, mean_squared_log_error
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler, LabelEncoder
+from sklearn.ensemble import GradientBoostingRegressor
 
 
-### Se cargan datos TRAIN
+
+### Se cargan datos TRAIN y se crea columna identificadora
 df_train = pd.read_csv("train.csv", encoding ="latin-1", sep=",", header=0)
+df_train['istrain']=1
 
-### Eliminar variables con missing values
-nulltrain=df_train.isnull().sum()
-nulltrain=nulltrain[nulltrain>0]
-nulltrain.sort_values(ascending=False)
-df_train = df_train.drop(columns=nulltrain.index.values)
-
-# Se separan variables númericas de las no-númericas(Puede contener categóricas)
-colsnum=df_train.select_dtypes(exclude = ["object"]).columns
-colsnum= colsnum.drop('SalePrice')
-x_num=df_train[colsnum]
-
-#x_num=x_num.fillna(x_num.median())
-skewness = x_num.apply(lambda x: skew(x))
-skewness.sort_values(ascending=False)
-skewness = skewness[abs(skewness)>0.5]
-
-#Analizar todos los histogramas y scatter de x_num con SalePrice
-for item in skewness.index.values:
-    sns.distplot(x_num[item])
-    plt.show()
-    data =pd.concat([df_train.SalePrice,x_num[item]],axis=1)
-    data.plot.scatter(y='SalePrice',x=item)
-    plt.show()
-    
-# Variables que tienen una relacion interesante con SalePrice    
-# GrLivArea 1stFlrSF 2ndFlrSF TotalBsmtSF BsmtFinSF1 YearBuilt
-# Se elimina el resto de las variables spuestamente skewed
-dropvar = ['MSSubClass','LotArea','OverallCond','YearRemodAdd','BsmtFinSF1',
-           'BsmtFinSF2','BsmtUnfSF','LowQualFinSF','BsmtFullBath','BsmtHalfBath',
-           'HalfBath','KitchenAbvGr','TotRmsAbvGrd','Fireplaces','WoodDeckSF',
-           'OpenPorchSF','EnclosedPorch','3SsnPorch','ScreenPorch','PoolArea',
-           'MiscVal']    
-x_num = x_num.drop(columns=dropvar)
-skewness = x_num.apply(lambda x: skew(x))
-skewness.sort_values(ascending=False)
-skewness = skewness[abs(skewness)>0.5]
-# Se redefine el conjunto skewness y se aplica la transformacion    
-x_num[skewness.index] = np.log1p(x_num[skewness.index])
-
-# Separación de variables no númericas, categóricas
-colscat=df_train.select_dtypes(include = ["object"]).columns
-x_cat = df_train[colscat]
-# Analizar Boxplots de x_cat con SalePrice
-for item in colscat:
-    data = pd.concat([df_train['SalePrice'], df_train[item]], axis=1)
-    f, ax = plt.subplots(figsize=(8, 6))
-    fig = sns.boxplot(x=item, y="SalePrice", data=data)
-    plt.show()
-
-# Se cargan datos TEST
+# Se cargan datos TEST y se crea columna identificadora
 df_test = pd.read_csv("test.csv",encoding="latin-1",sep=",",header=0)
+df_test['istrain']=0
 
-# Se eliminan las mismas columnas/variables eliminadas en TRAIN
-df_test = df_test.drop(columns=nulltrain.index.values)
-df_test =df_test.drop(columns=dropvar)
-
-# Se toman las variables númericas, imputan medianas y aplica log(skew[train])
-colsnum_test=df_test.select_dtypes(exclude = ["object"]).columns
-x_num_test=df_test[colsnum_test]
-x_num_test=x_num_test.fillna(x_num_test.median())  
-x_num_test[skewness.index] = np.log1p(x_num_test[skewness.index])
-
-# Se toman las variables no númericas
-colscat_test=df_test.select_dtypes(include = ["object"]).columns
-x_cat_test = df_test[colscat_test]
-
-# Se agrupan las variables
-x_train_=pd.concat([x_num,x_cat],axis=1)
-x_train_['istest']=0
-x_test_=pd.concat([x_num_test,x_cat_test],axis=1)
-x_test_['istest']=1
-tempo=pd.concat([x_train_,x_test_],axis=0)
-tempo=pd.get_dummies(tempo, prefix=colscat)
-x_train_=tempo[tempo['istest']==0]
-x_test_=tempo[tempo['istest']==1]
-###Transformar a distribucion normal SalePrice
+# Se guarda Y y se transforma
 y = df_train.SalePrice
 y=np.log(y)
 
-# Separación de TRAIN -- Train=Train ; Test=CrossValidation
+# Se unen TRAIN y TEST axis=0 en filas
+all_data=pd.concat([df_train,df_test],axis=0)
+all_data=all_data.drop(columns='SalePrice') 
+
+
+### Vista de missing values y reemplazo por valores
+nulltrain=all_data.isnull().sum()
+nulltrain=nulltrain[nulltrain>0]
+nulltrain.sort_values(ascending=False)
+for item in nulltrain.index:
+    if all_data[item].dtype=='O':
+        all_data[item]=all_data[item].fillna('None')
+    else:
+        all_data[item]=all_data[item].fillna(0)
+        
+## LabelEncoder        
+        
+cols = ('FireplaceQu', 'BsmtQual', 'BsmtCond', 'GarageQual', 'GarageCond', 
+        'ExterQual', 'ExterCond','HeatingQC', 'PoolQC', 'KitchenQual', 'BsmtFinType1', 
+        'BsmtFinType2', 'Functional', 'Fence', 'BsmtExposure', 'GarageFinish', 'LandSlope',
+        'LotShape', 'PavedDrive', 'Street', 'Alley', 'CentralAir', 'MSSubClass', 'OverallCond', 
+        'YrSold', 'MoSold')
+# process columns, apply LabelEncoder to categorical features
+for c in cols:
+    lbl = LabelEncoder() 
+    lbl.fit(list(all_data[c].values)) 
+    all_data[c] = lbl.transform(list(all_data[c].values))        
+        
+### Se agrega TotalSF
+
+all_data['TotalBsmtSF']=all_data['TotalBsmtSF'].astype('int64')
+
+all_data['TotalSF']= all_data['TotalBsmtSF'] + all_data['1stFlrSF'] + all_data['2ndFlrSF']         
+
+## Arreglar ciertas variables
+
+all_data['TotalSF']= all_data['TotalSF'].astype('float64') 
+
+all_data['BsmtFinSF1']=all_data['BsmtFinSF1'].astype('float64')
+
+all_data['BsmtFinSF2']=all_data['BsmtFinSF2'].astype('float64')  
+
+all_data['BsmtUnfSF']=all_data['BsmtUnfSF'].astype('float64')
+
+all_data['GarageArea']=all_data['GarageArea'].astype('float64')
+         
+all_data['LotFrontage']=all_data['LotFrontage'].astype('float64')
+
+all_data['MasVnrArea']=all_data['MasVnrArea'].astype('float64')
+##  Revision Skewness 
+# Se separan variables númericas de las no-númericas(Puede contener categóricas)
+
+colsnum=all_data.select_dtypes(exclude = ["object"]).columns
+x_num=all_data[colsnum]
+
+corrmat=pd.concat([x_num[x_num['istrain']==1],y],axis=1).corr()
+corrmat['SalePrice'].sort_values(ascending=False)##Revisar correlacion de TotBsmtSF
+ 
+
+## Se descartan las variables que no tienen relacion lineal con SalePrice
+
+dropvar=corrmat['SalePrice'][abs(corrmat['SalePrice'])<0.5].index.values
+x_num=x_num.drop(columns=dropvar)
+
+### Ahora se revisa Skewness
+
+skewness = x_num.apply(lambda x: skew(x))
+skewness.sort_values(ascending=False)
+skewness = skewness[abs(skewness)>0.75]
+    
+# Se redefine el conjunto skewness y se aplica la transformacion    
+x_num[skewness.index] = np.log1p(x_num[skewness.index]) #Revisar 
+
+
+## Separación de variables no númericas, categóricas
+colscat=all_data.select_dtypes(include = ["object"]).columns
+x_cat = all_data[colscat]
+x_cat=pd.get_dummies(x_cat, prefix=colscat)
+
+
+## Se vuelven a separar TRAIN y TEST
+all_data_pre=pd.concat([x_num,x_cat],axis=1)
+x_train_=all_data_pre[all_data_pre['istrain']==1]
+x_train_=x_train_.drop(columns='istrain')
+x_test_=all_data_pre[all_data_pre['istrain']==0]
+x_test_=x_test_.drop(columns='istrain')
+
+
+# Separación de TRAIN -- Train=Train ; Test=CrossValidation    
 x_train,x_test,y_train,y_test = train_test_split(x_train_,y,test_size=0.3, random_state=4)
 
 # Se crea la regresión y se ajustan los parámetros
@@ -117,27 +136,13 @@ reg=LinearRegression()
 reg.fit(x_train,y_train)
 
 predictions=reg.predict(x_test)
-print("Modelo regresión lineal sin regularización")
-#print('Coefficients: \n', reg.coef_)
-print("MAPE: %.2f"% mean_absolute_percentage_error(y_test, predictions),"%")
-print("RMSE: %.2f"% np.sqrt(mean_squared_error(y_test, predictions)))
-print("RMSLE: %.5f"% np.sqrt(mean_squared_log_error(y_test,predictions)))
 
-
-###### BIAS/VARIANCE THEORY ******ACCURACY****** ########
-
-print('Training (R^2) score: {}'.format(reg.score(x_train, y_train)))
-print('Test/Validation (R^2) score: {}'.format(reg.score(x_test, y_test)))
-
-##### Feature Scaler 
-#sc=StandardScaler()
-#x_train=sc.fit_transform(x_train)
-#x_test=sc.transform(x_test)#se requiere incluir fit solamente la primera vez que se utiliza
-
-##### Polynomial features
-#pf=PolynomialFeatures(degree=2)
-#x_train=pf.fit_transform(x_train)
-#x_test=pf.transform(x_test)#misma idea
+columnastabla=['Modelo','Training R^2', 'Test/Validation R^2', 'RMSLE','RMSE']
+Linear=['RL',
+        "%.4f" % reg.score(x_train,y_train),
+        "%.4f" % reg.score(x_test,y_test),
+        "%.4f" % np.sqrt(mean_squared_log_error(y_test,predictions)),
+        "%.4f" % np.sqrt(mean_squared_error(y_test, predictions))]
 
 
 ########## L2 Regularization or Ridge Regression 
@@ -148,61 +153,61 @@ ridge=RidgeCV(alphas = [0.01, 0.03, 0.06, 0.1, 0.3, 0.6, 1, 3, 6, 10, 30, 60], f
 ridge.fit(x_train, y_train)
 
 alpha=ridge.alpha_
-print('Best alpha', alpha)
+print('Best alpha',"%.2f" % alpha)
 
-print("Try again for more precision with alphas centered around " + str(alpha))
+print("Try again for more precision with alphas centered around " + "%.2f" %alpha)
 ridge = RidgeCV(alphas = [alpha * .6, alpha * .65, alpha * .7, alpha * .75, alpha * .8, alpha * .85, 
                           alpha * .9, alpha * .95, alpha, alpha * 1.05, alpha * 1.1, alpha * 1.15,
                           alpha * 1.25, alpha * 1.3, alpha * 1.35, alpha * 1.4],cv = 5, fit_intercept=True) 
 
 ridge.fit(x_train, y_train)
 alpha = ridge.alpha_
-print("Best alpha :", alpha)
+print("Best alpha :","%.2f" % alpha)
 
-print("Try again for more precision with alphas centered around " + str(alpha))
+print("Try again for more precision with alphas centered around " + "%.2f" % alpha)
 ridge = RidgeCV(alphas = [alpha * .6, alpha * .65, alpha * .7, alpha * .75, alpha * .8, alpha * .85, 
                           alpha * .9, alpha * .95, alpha, alpha * 1.05, alpha * 1.1, alpha * 1.15,
                           alpha * 1.25, alpha * 1.3, alpha * 1.35, alpha * 1.4],cv = 5, fit_intercept=True) 
 
 ridge.fit(x_train, y_train)
 alpha = ridge.alpha_
-print("Best alpha :", alpha)
+print("Best alpha :","%.2f" % alpha)
 
 test_pre = ridge.predict(x_test)
 train_pre = ridge.predict(x_train)
 
+
 coef = pd.Series(ridge.coef_)
-
-#plot between predicted values and residuals
-plt.scatter(train_pre, train_pre - y_train, c = "blue",  label = "Training data")
-plt.scatter(test_pre,test_pre - y_test, c = "black",  label = "Validation data")
-plt.title("Linear regression")
-plt.xlabel("Predicted values")
-plt.ylabel("Residuals")
-plt.legend(loc = "upper left")
-plt.hlines(y = 0, xmin = 10.5, xmax = 13.5, color = "red")
-plt.show()
-
-# Plot predictions - Real values
-plt.scatter(train_pre, y_train, c = "blue",  label = "Training data")
-plt.scatter(test_pre, y_test, c = "black",  label = "Validation data")
-plt.title("Linear regression with Ridge regularization")
-plt.xlabel("Predicted values")
-plt.ylabel("Real values")
-plt.legend(loc = "upper left")
-plt.plot([10.5, 13.5], [10.5, 13.5], c = "red")
-plt.show()
 
 print("Ridge picked " + str(sum(coef != 0)) + " variables and eliminated the other " +  str(sum(coef == 0)) + " variables")
 
-print("MAPE: %.2f"% mean_absolute_percentage_error(y_test, test_pre),"%")
-print("RMSE: %.2f"% np.sqrt(mean_squared_error(y_test, test_pre)))
-print("RMSLE: %.5f"% np.sqrt(mean_squared_log_error(y_test,test_pre)))
-print('Modelo Ridge')
+Ridge=['RL-Ridge:'+"%.2f" % alpha,
+        "%.4f" % ridge.score(x_train,y_train),
+        "%.4f" % ridge.score(x_test,y_test),
+        "%.4f" % np.sqrt(mean_squared_log_error(y_test,test_pre)),
+        "%.4f" % np.sqrt(mean_squared_error(y_test, test_pre))]
 
-print('Training (R^2) score: {}'.format(ridge.score(x_train, y_train)))
-print('Test/Validation (R^2) score: {}'.format(ridge.score(x_test, y_test)))
 
+### Gradient Boosting Regression ###
+GBoost = GradientBoostingRegressor(n_estimators=3000, learning_rate=0.05,
+                                   max_depth=4, max_features='sqrt',
+                                   min_samples_leaf=15, min_samples_split=10, 
+                                   loss='huber', random_state =5)
+GBoost.fit(x_train,y_train)
+test_pre=GBoost.predict(x_test)
+train_pre=GBoost.predict(x_train)
+
+
+GradBoost=['GBoost-R',
+        "%.4f" % GBoost.score(x_train,y_train),
+        "%.4f" % GBoost.score(x_test,y_test),
+        "%.4f" % np.sqrt(mean_squared_log_error(y_test,test_pre)),
+        "%.4f" % np.sqrt(mean_squared_error(y_test, test_pre))]
+
+#### Tabla con Resultados ######
+
+Resultados=pd.DataFrame(np.array([Linear,Ridge,GradBoost]),columns=columnastabla)
+print(tabulate(Resultados,headers='keys',tablefmt='fancy_grid'))
 ########Predicciones finales##########
 
 
